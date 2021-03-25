e5e0b30bd19cdc4e16cae9fe9ac4d5cc2f3a1268
rominavila47 <rominavila656@gmail.com>
2021-03-25 13:52:13 -0300

    buscador por id

--- src/main/java/com/ada/aerolinea/aerolineaAdaV1/repository/AirplaneRepository.java
+++ src/main/java/com/ada/aerolinea/aerolineaAdaV1/repository/AirplaneRepository.java
@@ -4,4 +4,5 @@ import com.ada.aerolinea.aerolineaAdaV1.model.Airplane;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AirplaneRepository extends CrudRepository<Airplane,Integer> {
+
 }
